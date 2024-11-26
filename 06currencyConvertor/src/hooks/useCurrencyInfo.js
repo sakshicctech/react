e9730731fcdefb248820afc754d6676f19009d9c
sakshicctech <sakshi.thorat@cctech.co.in>
2024-11-26 16:12:47 +0530

    feat: added functionality to currency convertor

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -5,7 +5,6 @@ const useCurrencyInfo = (currency) => {
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`).then(res => res.json()).then(res => setData(res[currency]))
     }, [currency])
-    console.log(data);
     return data
     
 }
